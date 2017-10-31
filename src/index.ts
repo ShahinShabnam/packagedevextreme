7ec38ca50d78b7ff14174281e7600055e054d75f
ShahinShabnam <shabnam.shahin@rancelab.com>
2017-10-31 10:10:39 +0530

    giving absolute path

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { SampleComponent } from './sample.component';
 import { SampleDirective } from './sample.directive';
 import { SamplePipe } from './sample.pipe';
 import { SampleService } from './sample.service';
-import { DxButtonModule } from 'devextreme-angular';
+import { DxButtonModule } from 'devextreme-angular/ui/button';
 export * from './sample.component';
 export * from './sample.directive';
 export * from './sample.pipe';
